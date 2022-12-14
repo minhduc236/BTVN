9dc23ea0c5d5e45dead35f7dae579c26996effc2
minhduc236 <118780716+minhduc236@users.noreply.github.com>
2022-12-14 13:04:44 +0700

    Add files via upload

---
+++ lap11.sql
@@ -0,0 +1,70 @@
+----cau 1----
+create function diemtb (@msv char(5))
+returns float
+as
+begin
+ declare @tb float
+ set @tb = (select avg(Diemthi)
+ from KetQua
+where MaSV=@msv)
+ return @tb
+end
+go
+select dbo.diemtb ('01')
+----cau 2-----
+----2a
+create function trbinhlop(@malop char(5))
+returns table
+as
+return
+ select s.masv, Hoten, trungbinh=dbo.diemtb(s.MaSV)
+ from Sinhvien s join KetQua k on s.MaSV=k.MaSV
+ where MaLop=@malop
+ group by s.masv, Hoten
+ ----2b
+create function trbinhlop1(@malop char(5))
+returns @dsdiemtb table (masv char(5), tensv nvarchar(20), dtb float)
+as
+begin
+ insert @dsdiemtb
+ select s.masv, Hoten, trungbinh=dbo.diemtb(s.MaSV)
+ from Sinhvien s join KetQua k on s.MaSV=k.MaSV
+ where MaLop=@malop
+ group by s.masv, Hoten
+ return
+end
+go
+select*from trbinhlop1('a')
+-----cau 3-----
+create proc ktra @msv char(5)
+as
+begin
+ declare @n int
+ set @n=(select count(*) from ketqua where Masv=@msv)
+ if @n=0
+ print 'sinh vien '+@msv + 'khong thi mon nao'
+ else
+ print 'sinh vien '+ @msv+ 'thi '+cast(@n as char(2))+ 'mon'
+end
+go
+exec ktra '01'
+-----cau 4-----
+create trigger updatesslop
+on sinhvien
+for insert
+as
+begin
+ declare @ss int
+ set @ss=(select count(*) from sinhvien s
+ where malop in(select malop from inserted))
+ if @ss>10
+ begin
+ print 'Lop day'
+ rollback tran
+ end
+ else
+ begin
+ update lop
+ set SiSo=@ss
+ where malop in (select malop from inserted)
+ end
