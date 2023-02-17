24ee929e76942128cfcc90d08bbdf16feb4fc0a2
minhduc236 <118780716+minhduc236@users.noreply.github.com>
2023-02-17 12:47:59 +0700

    Add files via upload

---
+++ buoi2-2-2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<html>
+    <body>
+    <table background = "../hoa/bg.jpg" border="1px" width = "400px" align="center">
+        <tbody>
+            <tr align = "center">
+                <tr>
+                <td><img src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmrhoa.com%2Fhoa-dep%2F&psig=AOvVaw2Zhj92C8rH28DY8yWlk6eO&ust=1676698985192000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLiFxtPsm_0CFQAAAAAdAAAAABAN" width="150px"><br>Hoa 1</td>
+                <h1><td rowspan="4">SẢN PHẨM GIỎ HOA KHUYẾN MÃI </td></h1>
+                <td><img src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fanvientv.com.vn%2Fhinh-anh-hoa-tulip&psig=AOvVaw2Zhj92C8rH28DY8yWlk6eO&ust=1676698985192000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLiFxtPsm_0CFQAAAAAdAAAAABAW" width="150px"><br>Hoa 2</td>
+                </tr>
+                <tr>
+                <td><img src = ".https://www.google.com/url?sa=i&url=https%3A%2F%2Fhoala.vn%2Fnhung-hinh-anh-dep-nhat-ve-hoa-hong-leo-kieu-sa.html&psig=AOvVaw2Zhj92C8rH28DY8yWlk6eO&ust=1676698985192000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLiFxtPsm_0CFQAAAAAdAAAAABAf" width="150px"><br>Hoa 3</td>
+                <td><img src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmisshoa.com%2Fhoa-dep%2Fhinh-anh-ve-hoa%2F&psig=AOvVaw2Zhj92C8rH28DY8yWlk6eO&ust=1676698985192000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLiFxtPsm_0CFQAAAAAdAAAAABAo" width="150px"><br>Hoa 4</td>
+                </tr>
+                <tr>
+                <td><img src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fhellohoa.com%2Fhoa-dep%2Fcam-tu-cau%2F&psig=AOvVaw2Zhj92C8rH28DY8yWlk6eO&ust=1676698985192000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLiFxtPsm_0CFQAAAAAdAAAAABAx" width="150px"><br>Hoa 5</td>
+                <td><img src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bachhoaxanh.com%2Fkinh-nghiem-hay%2Fy-nghia-hinh-anh-hoa-hong-trang-cach-trong-chi-tiet-tai-nha-1452630&psig=AOvVaw2Zhj92C8rH28DY8yWlk6eO&ust=1676698985192000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLiFxtPsm_0CFQAAAAAdAAAAABA6" width="150px"><br>Hoa 6</td>
+                </tr>
+                <tr>
+                <td><img src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bachhoaxanh.com%2Fkinh-nghiem-hay%2Fy-nghia-dac-biet-cua-loai-hoa-hong-den-hiem-co-va-noi-mua-hong-den-1370385&psig=AOvVaw2Zhj92C8rH28DY8yWlk6eO&ust=1676698985192000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLiFxtPsm_0CFQAAAAAdAAAAABBD" width="150px"><br>Hoa 7</td>
+                <td><img src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftip.edu.vn%2F25-hinh-nen-hoa-huong-duong-dep-nhat-the-gioi%2F&psig=AOvVaw2Zhj92C8rH28DY8yWlk6eO&ust=1676698985192000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLiFxtPsm_0CFQAAAAAdAAAAABBM" width="150px"><br>Hoa 8</td>
+                </tr>
+            </tr>
+        </tbody>
+    </table>
+    </body>
+</html>
