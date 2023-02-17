037c26ae567b9566846ef806c0b620121949b3ec
minhduc236 <118780716+minhduc236@users.noreply.github.com>
2023-02-17 08:48:19 +0700

    Add files via upload

---
+++ buoi2-1-1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <body style="background-color: yellow;">
+    <h2>CÁC MÈO CỦA NĂM</h2>
+    <img src="./IMG/meo1.jpg"alt="Italian Trulli" height = "450" width = "450">
+</br>
+    <img src="./IMG/meo2.jpg"alt="Italian Trulli" height = "450" width = "450">
+></br>
+    <img src="./IMG/meo3.jpg"alt="Italian Trulli" height = "450" width = "450">
+></br>
+    <img src="./IMG/meo4.jpg"alt="Italian Trulli" height = "450" width = "450">
+></br>
+    <img src="./IMG/meo5.jpg"alt="Italian Trulli" height = "450" width = "450">
+></br> 
+</body>
+</html>
